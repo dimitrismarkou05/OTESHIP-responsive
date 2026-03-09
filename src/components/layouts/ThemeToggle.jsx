fb3ad06dea40ca2e9ecc03b1996912ce611d33a2
ben10laden <armagedonioskatsikas@gmail.com>
2026-03-09 19:05:39 +0200

    fixed theme toggle button size

--- src/components/layouts/ThemeToggle.jsx
+++ src/components/layouts/ThemeToggle.jsx
@@ -14,14 +14,14 @@ const ThemeToggle = () => {
 
   return (
     <button
-      className="relative inline-block w-4 h-4 lg:w-5 lg:h-5 xl:w-8 xl:h-8 p-1.5 rounded-full hover:bg-gray-100 dark:hover:bg-(--color-secondary) transition-colors duration-200 overflow-hidden cursor-pointer"
+      className="relative inline-block shrink-0 w-6 h-6 xs:w-6.5 xs:h-6.5 sm:w-7 sm:h-7 md:w-7 md:h-7 lg:w-8 lg:h-8 xl:w-8 xl:h-8 rounded-full hover:bg-gray-100 dark:hover:bg-(--color-secondary) transition-colors duration-200 overflow-hidden cursor-pointer"
       style={{ lineHeight: 0 }}
       onClick={handleToggle}
       aria-label={isDarkMode ? "Switch to light mode" : "Switch to dark mode"}
     >
       {/* Moon - shows when in light mode */}
       <i
-        className={`ml-px fa-solid fa-moon text-xl text-(--color-secondary-light) absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+        className={`ml-px fa-solid fa-moon text-base sm:text-lg lg:text-xl text-(--color-secondary-light) absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
         transition-all duration-300 ease-out origin-center ${
           isDarkMode
             ? "opacity-0 scale-50 rotate-90"
@@ -31,7 +31,7 @@ const ThemeToggle = () => {
 
       {/* Sun - shows when in dark mode */}
       <i
-        className={`text-xl text-(--color-light3-text) absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+        className={`text-base sm:text-lg lg:text-xl text-(--color-light3-text) absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
         transition-all duration-300 ease-out origin-center ${
           isDarkMode
             ? "opacity-100 scale-100 rotate-0"
@@ -42,8 +42,8 @@ const ThemeToggle = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          width="1.4em"
-          height="1.4em"
+          width="1.3em"
+          height="1.3em"
         >
           <path
             d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
