ac67f485e7bd29df16d1e878d9f9a29db42e0be7
ben10laden <armagedonioskatsikas@gmail.com>
2026-03-13 16:05:14 +0200

    changed the school card to Link removed image hover effect modified card hover effect

--- src/components/common/SchoolCard.jsx
+++ src/components/common/SchoolCard.jsx
@@ -15,17 +15,17 @@ const SchoolCard = ({
       data-aos="fade-down"
       data-aos-delay={aosDelay}
     >
-      <div className="flex flex-col md:flex-row lg:flex-col bg-white dark:bg-(--color-dark2-text) drop-shadow-md rounded-md min-w-0 flex-1 transition-colors duration-200 h-full">
-        <Link
-          to={to}
-          className="inset-0 block w-full aspect-video overflow-hidden rounded-t-md md:w-1/2 md:rounded-none md:rounded-l-md lg:w-full lg:rounded-none lg:rounded-t-md"
-        >
+      <Link
+        to={to}
+        className="flex flex-col md:flex-row lg:flex-col bg-white dark:bg-(--color-dark2-text) drop-shadow-md rounded-md min-w-0 flex-1 [transition:all_200ms,translate_300ms] hover:shadow-xl/4 hover:-translate-y-1 h-full"
+      >
+        <div className="inset-0 block w-full aspect-video overflow-hidden rounded-t-md md:w-1/2 md:rounded-none md:rounded-l-md lg:w-full lg:rounded-none lg:rounded-t-md">
           <img
             src={image}
             alt="Hands-On Learning"
-            className="w-full h-full object-cover transition-transform duration-400 ease-in-out hover:scale-105"
+            className="w-full h-full object-cover"
           />
-        </Link>
+        </div>
         <div className="flex flex-col justify-start items-start gap-1.5 xs:gap-2 md:gap-2.5 flex-1 p-3 xs:p-3.5 md:p-4 xl:p-5">
           <h1 className="text-sm md:text-base lg:text-lg font-bold text-(--color-dark-text) dark:text-white w-full line-clamp-2">
             {title}
@@ -43,7 +43,7 @@ const SchoolCard = ({
             </p>
           </Link>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
